b8d302d41684d2b1e9383c30cbeccdb71c90a40d
qunqun-io <Danny0@qunqun.io>
2018-03-12 09:38:24 +0800

    use sha3 of site name as key

--- QunQunCommunity.sol
+++ QunQunCommunity.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+import {SafeMath} from "./SafeMath.sol";
 
 contract ERC20 {
     address public owner;
@@ -8,33 +9,36 @@ contract ERC20 {
 }
 
 contract QunQunCommunity {
+    
+    using SafeMath for uint256;
 
-    //community struct
     struct Community {
-        bool is_exist;
-        bytes32 key;  //community key, may be used as subdomain
+        string name;
+        bool isExist;
         uint8 status; //0: shut, 1: running, 2: for_sale
         address owner;
         address manager; //manager has the same privilege as owner
-        uint256 price; // sales price
+        uint256 salePrice; // sales price
         uint256 timestamp;
         uint256 number; //auto increment number
+        string extendInfo; //may be a ipfs link
     }
     
     uint256 public lastBlockNumber = 0;
     uint256 public lastCommunityNumber = 10000;
     uint256 public blockInterval = 24; // 24 blocks is about 6 minutes
-    uint256 public setUpPrice = 4999 * 10 ** 16; //init price 49.99QUN
+    uint256 public setUpFee = 49.99 ether; //init price 49.99QUN
+    uint256 public exchangeFeePercent = 5; //sale Community fee
     address public qunTokenContractAddress;
-    address public owner;
+    address public contractOwner;
 
     mapping (bytes32 => Community) public communities; //community list, site name as key
-    //for save site keys, site_key and site_name both need be unique
-    mapping (bytes32 => bytes32) public communityKeys;
     
+    event Create(address indexed _from, address indexed _owner, string _name);
+    event Transfer(address indexed _from, address indexed _to, bytes32 _name);
     
     function QunQunCommunity(address _tokenContractAddress) public{
-        owner = msg.sender;
+        contractOwner = msg.sender;
         lastBlockNumber = block.number;
         qunTokenContractAddress = _tokenContractAddress;
     }
@@ -42,151 +46,176 @@ contract QunQunCommunity {
     /**
     * try create new community
     */
-    function createSite(bytes32 siteName, address siteOwner, address siteManager) public returns (bool success) {
-        require(block.number - lastBlockNumber > blockInterval);
+    function createSite(string _name, address _owner, address _manager) public returns (bool success) {
+        require(block.number - lastBlockNumber >= blockInterval);
+        require(bytes(_name).length <= 64);
+        bytes32 nameHash = keccak256(_name);
         //check the key is exist
-        require(communities[siteName].is_exist == false);
+        require(communities[nameHash].isExist == false);
         ERC20 qunTokenContract = ERC20(qunTokenContractAddress);
         //check balances
-        require(qunTokenContract.allowance(msg.sender, address(this)) >= setUpPrice);
-        if(siteOwner == 0x0){
-            siteOwner = msg.sender;
+        require(qunTokenContract.allowance(msg.sender, address(this)) >= setUpFee);
+        if(_owner == 0x0){
+            _owner = msg.sender;
         }
         //set community info
-        Community memory newSite =  Community({
-            is_exist: true,
-            key: '',
+        communities[nameHash] = Community({
+            name: _name,
+            isExist: true,
             status: 1,
-            owner: siteOwner,
-            manager: siteManager,
-            price: 0,
+            owner: _owner,
+            manager: _manager,
+            salePrice: 0,
             timestamp: block.timestamp,
-            number: lastCommunityNumber + 1
+            number: lastCommunityNumber + 1,
+            extendInfo: ""
         });
-        communities[siteName] = newSite;
         lastCommunityNumber += 1;
-        //transfer QUN
-        qunTokenContract.transferFrom(msg.sender, qunTokenContract.owner(), setUpPrice);
+        //transfer set up fee
+        qunTokenContract.transferFrom(msg.sender, contractOwner, setUpFee);
         lastBlockNumber = block.number;
+        Create(msg.sender, _owner, _name);
         return true;
     }
     
     
-    modifier checkAuth(bytes32 siteName){
+    modifier checkAuth(bytes32 _name){
         //is owner or manager
-        require(communities[siteName].owner == msg.sender || communities[siteName].manager == msg.sender);
+        require(communities[_name].owner == msg.sender || communities[_name].manager == msg.sender);
         _;
     }
+    
 
     /**
     * sale community
     */
-    function setForSale(bytes32 siteName, uint256 price) checkAuth(siteName) public returns (bool success) {
+    function setForSale(bytes32 _name, uint256 _price) checkAuth(_name) public returns (bool success) {
+        require(_price >= setUpFee);
         //set status to sale
-        communities[siteName].status = 2;
-        communities[siteName].price = price;
+        communities[_name].status = 2;
+        communities[_name].salePrice = _price;
         return true;
     }
 
     /**
     * cancel sale
     */
-    function cancelSale(bytes32 siteName) checkAuth(siteName) public returns (bool success) {
+    function cancelSale(bytes32 _name) checkAuth(_name) public returns (bool success) {
         //set status to running
-        communities[siteName].status = 1;
-        communities[siteName].price = 0;
+        communities[_name].status = 1;
+        communities[_name].salePrice = 0;
         return true;
     }
     
     /**
     * shut down site
     */
-    function shutDown(bytes32 siteName) checkAuth(siteName) public returns (bool) {
+    function shutDown(bytes32 _name) checkAuth(_name) public returns (bool) {
         //set status to shut down
-        communities[siteName].status = 0;
+        communities[_name].status = 0;
         return true;
     }
     
     /**
     * open site
     */
-    function openAgain(bytes32 siteName) checkAuth(siteName) public returns (bool) {
+    function openAgain(bytes32 _name) checkAuth(_name) public returns (bool) {
         //set status to running
-        communities[siteName].status = 1;
+        communities[_name].status = 1;
         return true;
     }
 
     /**
     * change sales price
     */
-    function changePrice(bytes32 siteName, uint256 newPrice) checkAuth(siteName) public returns (bool) {
-        //check the community is on offer
-        require(communities[siteName].status == 2);
+    function changeSalePrice(bytes32 _name, uint256 _price) checkAuth(_name) public returns (bool) {
         //set new price
-        communities[siteName].price = newPrice;
+        communities[_name].salePrice = _price;
         return true;
     }
+    
 
     /**
     * buy community
     */
-    function buy(bytes32 siteName, address siteOwner) public returns (bool) {
+    function buy(bytes32 _name, address _owner, address _manager) public returns (bool) {
         //check the community is on offer
-        require(communities[siteName].status == 2);
-        if(siteOwner == 0x0){
-            siteOwner = msg.sender;
+        require(communities[_name].status == 2);
+        if(_owner == 0x0){
+            _owner = msg.sender;
         }
         ERC20 qunTokenContract = ERC20(qunTokenContractAddress);
         //check buyer balances
-        require(qunTokenContract.allowance(msg.sender, address(this)) >= communities[siteName].price);
-        //transfer money
-        qunTokenContract.transferFrom(msg.sender, communities[siteName].owner, communities[siteName].price);
+        require(qunTokenContract.allowance(msg.sender, address(this)) >= communities[_name].salePrice);
+        //transfer money to owner
+        uint256 income = communities[_name].salePrice.mul(100 - exchangeFeePercent).div(100);
+        qunTokenContract.transferFrom(msg.sender, communities[_name].owner, income);
+        //transfer exchange fee to qun platform
+        if(income < communities[_name].salePrice){
+            qunTokenContract.transferFrom(msg.sender, contractOwner, communities[_name].salePrice.sub(income));
+        }
+        Transfer(communities[_name].owner, _owner, _name);
         //change owner
-        communities[siteName].owner = siteOwner;
+        communities[_name].owner = _owner;
+        communities[_name].manager = _manager;
         //reset sale info
-        communities[siteName].status = 1;
-        communities[siteName].price = 0;
+        communities[_name].status = 1;
         return true;
     }
     
-    /**
-    * set community key, it can not change
-    */
-    function setCommunityKey(bytes32 siteName, bytes32 siteKey) checkAuth(siteName) public returns (bool){
-        //can only once
-        require(communityKeys[siteKey] == "");
-        communityKeys[siteKey] = siteName;
-        communities[siteName].key = siteKey;
-        return true;
+    function ownerOf(bytes32 _name) public view returns (address) {
+        return communities[_name].owner;
     }
+    
+    function extendInfo(bytes32 _name) public view returns (string){
+        return communities[_name].extendInfo;
+    }
+    
     
     /**
     * set manager
     */
-    function setManager(bytes32 siteName, address newManager) checkAuth(siteName) public returns (bool){
-        communities[siteName].manager = newManager;
+    function approve(address _to, bytes32 _name) checkAuth(_name) public returns (bool){
+        communities[_name].manager = _to;
         return true;
     }
+    
     
     /**
     * direct transfer community owner
     */
-    function transferOwner(bytes32 siteName, address newOwner) public returns (bool){
-        require(communities[siteName].owner == msg.sender);
-        communities[siteName].owner = newOwner;
+    function transfer(address _to, bytes32 _name) public returns (bool){
+        require(communities[_name].owner == msg.sender);
+        ERC20 qunTokenContract = ERC20(qunTokenContractAddress);
+        uint256 lastPrice = 0;
+        if(communities[_name].salePrice > setUpFee){
+            lastPrice = communities[_name].salePrice;
+        }else{
+            lastPrice = setUpFee;
+        }
+        qunTokenContract.transferFrom(msg.sender, contractOwner, lastPrice.mul(exchangeFeePercent).div(100));
+        Transfer(communities[_name].owner, _to, _name);
+        communities[_name].owner = _to;
         return true;
     }
     
+    modifier onlyContractOwner(){
+        require(contractOwner == msg.sender);
+        _;
+    }
     
-    function changeSetUpPrice(uint256 newPrice) public returns (bool){
-        require(owner == msg.sender);
-        setUpPrice = newPrice;
+    function changeSetUpFee(uint256 newFee) onlyContractOwner public returns (bool){
+        setUpFee = newFee;
         return true;
     }
     
-    function changeBlockInterval(uint256 newInterval) public returns (bool){
-        require(owner == msg.sender);
+    function changeBlockInterval(uint256 newInterval)  onlyContractOwner public returns (bool){
         blockInterval = newInterval;
+        return true;
+    }
+    
+    function changeExchangeFeePercent(uint256 newFeePercent) onlyContractOwner public returns (bool){
+        exchangeFeePercent = newFeePercent;
         return true;
     }
 
